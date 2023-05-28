6f888d713cba0002f1875a7e79cf6719832fbb9f
Oscar Andres Montes <oscarandresmontes@gmail.com>
2023-05-27 20:02:02 -0500

    API Testing

--- APITestingSession.py
+++ APITestingSession.py
@@ -8,3 +8,8 @@ url = 'https://api.github.com/user/repos'
 response = se.get(url)
 
 print(response.status_code)
+
+cookie = {'visit-month': 'February'}
+res = requests.get('http://rahulshettyacademy.com', cookies=cookie)
+
+print(res.status_code)
